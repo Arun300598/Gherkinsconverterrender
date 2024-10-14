cba2787196859b12366384b024c2bb6c8f421c94
Arun300598 <arunlakshmanmech@gmail.com>
2024-10-14 21:01:25 +0530

    Fixed bugs in the Gherkin to Java converter

--- app.py
+++ app.py
@@ -71,5 +71,5 @@ def index():
 
     return render_template('index.html', gherkin_text=gherkin_text, output=output)
 
-#if __name__ == '__main__':
-    #app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
